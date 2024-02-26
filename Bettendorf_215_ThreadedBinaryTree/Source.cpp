d8b62799ddc0a0ce171bd2802655591f48d0b273
Luke Bettendorf <lmbettendorf@liberty.edu>
2024-02-26 13:38:55 -0500

    Created left and right bitfields for the BSTNode.h with getters and setters

---
+++ Bettendorf_215_ThreadedBinaryTree/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "BST.h"
+#include "BSTNode.h"
+
+using namespace std;
+
+// Luke Bettendorf
+//CSCN 215 (003) - Spring 2024
+//Programming Assignment 2
+//Last Modified: 2/25/2024
+
+int main() {
+
+	
+
+	return 0;
+}
